d2bcc4beb369052102f018805a694d161497956a
Kristaps <kbebris+git@gmail.com>
2020-04-27 17:06:02 +0300

    updated the download script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # first thing's first clone stag if it's not here already
-git clone https://github.com/lucren/stag-mwc.git
+git clone https://github.com/lucren/stag-mwc.git ../stag-mwc
 
 # go into the folder and pull changes (mostly to deal with a case where the repohas already been pulled)
-cd stag-mwc
+cd ../stag-mwc
+git checkout hpc_mod # the modded branch that we're working with
 git pull
