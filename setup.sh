8f9a80ea3a5a7ec177ee37be132326528166e597
Kristaps <kbebris+git@gmail.com>
2020-04-27 16:51:58 +0300

    pull stub

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# first thing's first clone stag if it's not here already
+git clone https://github.com/lucren/stag-mwc.git
+
+# go into the folder and pull changes (mostly to deal with a case where the repohas already been pulled)
+cd stag-mwc
+git pull
