2aa56d392471b433dca1100c5e625d4fb44a02b7
Kristaps <kbebris+git@gmail.com>
2020-04-29 19:37:27 +0300

    hopefully fixed up taxon scripts - will need to check once the databases finish building

---
+++ local/run.taxon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# https://github.com/conda/conda/issues/7980
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate stag-mwc
+# run through all of our built up taxon jobs
+cd ../..
+for f in process_taxon_*; do
+    cd "$f/stag-mwc"
+    snakemake --use-conda --cores 12
+    cd ../..
+done
