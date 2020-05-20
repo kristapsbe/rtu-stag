7025af1abacb1082d85c6c6de048a796f34e146c
Kristaps <kbebris+git@gmail.com>
2020-05-20 17:56:04 +0300

    added separate setup func for the cluster

--- hpc/setup.core.sh
+++ hpc/setup.core.sh
@@ -4,5 +4,31 @@
 #PBS -l walltime=4:00:00
 #PBS -q long
 #PBS -j oe
-cd rtu-stag/local # move back into our project
-./setup.core.sh # run the same setup script that the local instance would use
+
+# clear out the old installation folders
+cd ../..
+rm -rf stag-mwc
+
+# first thing's first clone stag if it's not here already
+git clone https://github.com/lucren/stag-mwc.git stag-mwc
+
+# go into the folder and pull changes (mostly to deal with a case where the repohas already been pulled)
+cd stag-mwc
+git checkout hpc_mod # the modded branch that we're working with
+git pull
+
+# create a conda env 
+# pipe yes to overwrite the env
+echo "y" | conda create --name stag-mwc python=3
+conda activate stag-mwc
+
+# and add needed channels
+#
+# order matters - https://forum.biobakery.org/t/metaphlan3-installation-fails/350/2
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+
+# at this point we've cloned stag and need to run their setup process - conda is expected to be installed already
+# pipe yes into the install to silence prompts
+echo "y" | conda install -c bioconda -c conda-forge snakemake==5.5.4
