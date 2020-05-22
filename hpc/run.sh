71c283af5f27cf4d780775fd01a1bd2bc3f4f8ce
Kristaps <kbebris+git@gmail.com>
2020-05-22 19:25:44 +0300

    fixed up the run scripts

--- hpc/run.sh
+++ hpc/run.sh
@@ -5,5 +5,5 @@ cd ~/rtu-stag/hpc/subscripts
 
 for f in ~/rtu-stag/samples/*_1.fq.gz; do # don't want to trigger twice - limiting myself to the first file of the pair
     sample=$(echo $f | grep -o '[0-9]\+_[0-9]\+\.fq\.gz' | grep -o '[0-9]\+_' | grep -o '[0-9]\+')
-    qsub sub.run.sh $sample # create jobs for all of the samples
-    
+    qsub sub.run.sh -F "$sample" # create jobs for all of the samples
+done
