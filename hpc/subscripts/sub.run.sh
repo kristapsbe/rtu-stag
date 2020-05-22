01fcc6e418e264e909db5a505d07ae3363502ad9
Kristaps <kbebris+git@gmail.com>
2020-05-22 20:42:33 +0300

    debug code

--- hpc/subscripts/sub.run.sh
+++ hpc/subscripts/sub.run.sh
@@ -31,12 +31,14 @@ ls "$f/databases"
 cp -r "${home_path}/stag-mwc" "$f"
 cp "${home_path}/rtu-stag/configs/config.hpc.yaml" "$f/stag-mwc/config.yaml" # changing the name to the default simplifies running
 mkdir "$f/stag-mwc/input"
-for fname in "${home_path}/rtu-stag/samples/*_${sample}_*.fq.gz"; do # move both sample files
+for fname in ${home_path}/rtu-stag/samples/*_${sample}_*.fq.gz; do # move both sample files
     trimmed=$(echo $fname | grep -o '[0-9]\+_[0-9]\+\.fq\.gz')
 
     echo "$trimmed"
 
     cp $fname "$f/stag-mwc/input/$trimmed"
+
+    echo "$f/stag-mwc/input/$trimmed"
 done
 
 cp -r "${home_path}/kraken2" "$f"
