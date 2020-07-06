82576b5a16c158c4d58b0ed0dae3f63029d8859b
Kristaps <kbebris+git@gmail.com>
2020-07-06 19:26:18 +0300

    added checksum gathering script

--- local/bash_utils/generate_checksums.sh
+++ local/bash_utils/generate_checksums.sh
@@ -1,12 +1,3 @@
-# we've encountered a rather annoying problem in that files may get slightly crippled while getting transferred over the network
-#
-# we need to ensure that they make it over in one piece
-# current plan is:
-#         fetch all files
-#         verify that everything opens
-#         save checksums to file that will be uploaded along with the samples
-#         check against checksums after downloading to ensure that samples have not been crippled
-
 cd ../../samples
 
 rm checksums.txt
@@ -14,5 +5,12 @@ touch checksums.txt
 
 for f in *.fq.gz
 do
-    shasum -a512 $f >> checksums.txt
+    gunzip -k $f
+
+    for i in *.fq
+    do
+        shasum -a512 "$i.gz" >> checksums.txt
+    done
+
+    rm *.fq
 done
